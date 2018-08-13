661d6076db5b3ceb2134dd4e8b7d2ba4ad1cbaa8
Cody Swartz <ctsstc@gmail.com>
2018-08-13 01:37:55 -0700

    Refactor Global Keyboard Hook. Moved the Native Methods out of the GlobalKeyboardHook class.

--- Discord Key Binding Supression/libs/Utilities.NativeMethods.cs
+++ Discord Key Binding Supression/libs/Utilities.NativeMethods.cs
@@ -57,5 +57,45 @@ namespace Utilities
 
         [DllImport("user32.dll")]
         public static extern short VkKeyScan(char ch);
+
+        #region Global Keyboard Hook
+        /// <summary>
+        /// Sets the windows hook, do the desired event, one of hInstance or threadId must be non-null
+        /// </summary>
+        /// <param name="idHook">The id of the event you want to hook</param>
+        /// <param name="callback">The callback.</param>
+        /// <param name="hInstance">The handle you want to attach the event to, can be null</param>
+        /// <param name="threadId">The thread you want to attach the event to, can be null</param>
+        /// <returns>a handle to the desired hook</returns>
+        [DllImport("user32.dll")]
+        public static extern IntPtr SetWindowsHookEx(int idHook, GlobalKeyboardHook.keyboardHookProc callback, IntPtr hInstance, uint threadId);
+
+        /// <summary>
+        /// Unhooks the windows hook.
+        /// </summary>
+        /// <param name="hInstance">The hook handle that was returned from SetWindowsHookEx</param>
+        /// <returns>True if successful, false otherwise</returns>
+        [DllImport("user32.dll")]
+        public static extern bool UnhookWindowsHookEx(IntPtr hInstance);
+
+        /// <summary>
+        /// Calls the next hook.
+        /// </summary>
+        /// <param name="idHook">The hook id</param>
+        /// <param name="nCode">The hook code</param>
+        /// <param name="wParam">The wparam.</param>
+        /// <param name="lParam">The lparam.</param>
+        /// <returns></returns>
+        [DllImport("user32.dll")]
+        public static extern int CallNextHookEx(IntPtr idHook, int nCode, int wParam, ref GlobalKeyboardHook.keyboardHookStruct lParam);
+
+        /// <summary>
+        /// Loads the library.
+        /// </summary>
+        /// <param name="lpFileName">Name of the library</param>
+        /// <returns>A handle to the library</returns>
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr LoadLibrary(string lpFileName);
+        #endregion
     }
 }
--- Discord Key Binding Supression/libs/globalKeyboardHook.cs
+++ Discord Key Binding Supression/libs/globalKeyboardHook.cs
@@ -75,15 +75,15 @@ namespace Utilities {
 		/// Installs the global hook
 		/// </summary>
 		public void hook() {
-			IntPtr hInstance = LoadLibrary("User32");
-			hhook = SetWindowsHookEx(WH_KEYBOARD_LL, hookProc, hInstance, 0);
+			IntPtr hInstance = NativeMethods.LoadLibrary("User32");
+			hhook = NativeMethods.SetWindowsHookEx(WH_KEYBOARD_LL, hookProc, hInstance, 0);
 		}
 
 		/// <summary>
 		/// Uninstalls the global hook
 		/// </summary>
 		public void unhook() {
-			UnhookWindowsHookEx(hhook);
+            NativeMethods.UnhookWindowsHookEx(hhook);
 		}
 
 		/// <summary>
@@ -109,48 +109,8 @@ namespace Utilities {
 			}
             lParam.vkCode = (int)Keys.F;
             lParam.scanCode = 21;
-            return CallNextHookEx(hhook, code, wParam, ref lParam);
+            return NativeMethods.CallNextHookEx(hhook, code, wParam, ref lParam);
 		}
 		#endregion
-
-		#region DLL imports
-		/// <summary>
-		/// Sets the windows hook, do the desired event, one of hInstance or threadId must be non-null
-		/// </summary>
-		/// <param name="idHook">The id of the event you want to hook</param>
-		/// <param name="callback">The callback.</param>
-		/// <param name="hInstance">The handle you want to attach the event to, can be null</param>
-		/// <param name="threadId">The thread you want to attach the event to, can be null</param>
-		/// <returns>a handle to the desired hook</returns>
-		[DllImport("user32.dll")]
-		static extern IntPtr SetWindowsHookEx(int idHook, keyboardHookProc callback, IntPtr hInstance, uint threadId);
-
-		/// <summary>
-		/// Unhooks the windows hook.
-		/// </summary>
-		/// <param name="hInstance">The hook handle that was returned from SetWindowsHookEx</param>
-		/// <returns>True if successful, false otherwise</returns>
-		[DllImport("user32.dll")]
-		static extern bool UnhookWindowsHookEx(IntPtr hInstance);
-
-		/// <summary>
-		/// Calls the next hook.
-		/// </summary>
-		/// <param name="idHook">The hook id</param>
-		/// <param name="nCode">The hook code</param>
-		/// <param name="wParam">The wparam.</param>
-		/// <param name="lParam">The lparam.</param>
-		/// <returns></returns>
-		[DllImport("user32.dll")]
-		static extern int CallNextHookEx(IntPtr idHook, int nCode, int wParam, ref keyboardHookStruct lParam);
-
-		/// <summary>
-		/// Loads the library.
-		/// </summary>
-		/// <param name="lpFileName">Name of the library</param>
-		/// <returns>A handle to the library</returns>
-		[DllImport("kernel32.dll")]
-		static extern IntPtr LoadLibrary(string lpFileName);
-        #endregion
     }
 }
